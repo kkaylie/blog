95ac72263c605bd72243c34c46d45db2e73d0bca
kkaylie <kkaylie778@gmail.com>
2025-07-29 02:09:15 -0700

    chore(seo): update dynamic domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,9 @@ import { getPublishedPosts } from '@/lib/notion'
 import type { MetadataRoute } from 'next'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://your-domain.com' // TODO
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
 
   const posts = await getPublishedPosts()
 
