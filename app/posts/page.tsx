ea591a3e9a66c7ff62b798fe6234d4a525d3cb8a
kkaylie <kkaylie778@gmail.com>
2025-08-11 16:12:09 -0700

    fix: cache bug

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,6 +2,8 @@ import { getPublishedPosts } from '@/lib/notion'
 
 import { ArticleListItem } from '../components/ArticleListItem'
 
+export const revalidate = 60
+
 export default async function PostsList() {
   const posts = await getPublishedPosts()
   return posts.map((post) => <ArticleListItem key={post.slug} post={post} />)
