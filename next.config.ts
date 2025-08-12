b0ff858907ae73deee062418f829b07c09d5ffb8
kkaylie <kkaylie778@gmail.com>
2025-08-12 13:42:27 -0700

    chore: update redirect configs

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/',
-        destination: '/posts',
+        destination: '/about',
         permanent: true,
       },
     ]
